6fd644b5f1fbdc0311e179fe985612c5d290de3f
olemp <olemp@puzzlepart.com>
2018-08-27 13:22:10 +0200

    Added function CreateSite that creates a new site collection with web template STS#3

---
+++ Pzl.O365.ProvisioningFunctions/SharePoint/CreateSite.cs
@@ -0,0 +1,90 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Online.SharePoint.TenantAdministration;
+using Microsoft.Online.SharePoint.TenantManagement;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Sites;
+using Pzl.O365.ProvisioningFunctions.Helpers;
+
+namespace Pzl.O365.ProvisioningFunctions.SharePoint
+{
+    public static class CreateSite
+    {
+        [FunctionName("CreateSite")]
+        [ResponseType(typeof(CreateSiteResponse))]
+        [Display(Name = "Creates a modern team site", Description = "")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post")]CreateSiteRequest request, TraceWriter log)
+        {
+            string adminUrl = $"https://{request.Tenant}-admin.sharepoint.com";
+
+            try
+            {
+                var clientContext = await ConnectADAL.GetClientContext(adminUrl, log);
+                Tenant tenant = new Tenant(clientContext);
+                clientContext.ExecuteQuery();
+                string url = $"https://{request.Tenant}.sharepoint.com/sites/{request.Url}";
+                var siteCreationProperties = new SiteCreationProperties()
+                {
+                    Url = url,
+                    Owner = request.OwnerEmail,
+                    Template = "STS#3",
+                    StorageMaximumLevel = 100,
+                    UserCodeMaximumLevel = 0,
+                    Title = request.Title
+
+                };
+                tenant.CreateSite(siteCreationProperties);
+                clientContext.ExecuteQuery();
+
+                return await Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ObjectContent<CreateSiteResponse>(new CreateSiteResponse { SiteURL = url }, new JsonMediaTypeFormatter())
+                });
+            }
+            catch (Exception e)
+            {
+                log.Error($"Error:  {e.Message }\n\n{e.StackTrace}");
+                return await Task.FromResult(new HttpResponseMessage(HttpStatusCode.ServiceUnavailable)
+                {
+                    Content = new ObjectContent<string>(e.Message, new JsonMediaTypeFormatter())
+                });
+            }
+        }
+
+        public class CreateSiteRequest
+        {
+            [Required]
+            [Display(Description = "Tenant")]
+            public string Tenant { get; set; }
+            [Required]
+            [Display(Description = "Title ")]
+            public string Title { get; set; }
+            [Required]
+            [Display(Description = "Url")]
+            public string Url { get; set; }
+            [Display(Description = "Description ")]
+            public string Description { get; set; }
+            [Display(Description = "OwnerEmail ")]
+            public string OwnerEmail { get; set; }
+            [Display(Description = "Classification ")]
+            public string Classification { get; set; }
+            [Display(Description = "IsPublic   ")]
+            public bool IsPublic { get; set; }
+        }
+
+        public class CreateSiteResponse
+        {
+
+            [Display(Description = "URL of site")]
+            public string SiteURL { get; set; }
+        }
+    }
+}
